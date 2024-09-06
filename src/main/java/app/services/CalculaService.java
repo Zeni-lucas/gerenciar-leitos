06b0727e70b722201f7c545e1c1569cc0ce24d53
zeni-lucas <zeni.lucas@gmail.com>
2024-09-06 16:50:54 -0300

    metodos adicionais e testes

---
+++ src/main/java/app/services/CalculaService.java
@@ -0,0 +1,53 @@
+package app.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CalculaService {
+
+    public int soma(List<Integer> numeros){
+        int soma = 0;
+        for (Integer numero : numeros){
+            soma += numero;
+        }
+        return soma;
+    }
+
+    public double media(List<Integer> numeros){
+        if (numeros.isEmpty()){
+            return 0.0;
+        }
+        int soma = soma(numeros);
+        return (double) soma / numeros.size();
+    }
+
+    public int encontrarMaior(List<Integer> numeros) {
+        if (numeros.isEmpty()) {
+            System.out.println("A lista está vazia.");
+        }
+
+        int maior = numeros.get(0);
+        for (Integer numero : numeros) {
+            if (numero > maior) {
+                maior = numero;
+            }
+        }
+        return maior;
+    }
+
+    public int encontrarMenor(List<Integer> numeros) {
+        if (numeros.isEmpty()) {
+            System.out.println("A lista está vazia.");
+        }
+
+        int menor = numeros.get(0);
+        for (Integer numero : numeros) {
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+        return menor;
+    }
+}
